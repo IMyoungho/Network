68faa1fa9c1e294a6a083dc66b5df60d9fefe963
IMyoungho <sladkwnlwnl@gmail.com>
2021-06-22 22:07:25 +0900

    Inbound Outbound 선택기능 추가

--- easyDump/easyDump.sh
+++ easyDump/easyDump.sh
@@ -4,12 +4,11 @@ dst_ip=$2
 port_=$3
 para_cnt=$#
 
+#깨끗하게 지우고 시작
+clear
 
-
-# 첫번째 인자를 인터페이스를 넣기
-# -f : find interface
-# => -f 옵션을 쓸때랑 다른 옵션쓸때랑 인자 다르게 만들어야함
-
+echo ""
+echo " easyDump [Copyright(c) 2021 By IMyoungho]  ";
 # in(inbound) : using input = 출발지 아이피 인터페이스
 # out(outbound) : using output = 목적지 아이피 인터페이스
 
@@ -34,6 +33,8 @@ fi
 # 해당인터페이스가 어떤 존인지 fw show conf | grep Z
 
 # show ip & port
+echo ""
+echo "============================================";
 echo "";
 echo "  [+] Source Ip = $src_ip";
 echo "  [+] Destin Ip = $dst_ip";
@@ -44,18 +45,36 @@ elif [ $port_ -ge 1 ] && [ $port_ -le 65535 ]; then
   echo "  [+] Port = $port_";
 else
   echo "  [!] Check the port range < 1 - 65535 > :(";
+  echo ""
   exit 0
 fi
-echo "";
 
-echo "   [?] Choose inbound or outbound < Enter \"in\" or \"out\" >";
-read target
+
+echo "";
+echo "--------------------------------------------";
+echo "";
+# 인터페이스 입력받기
+echo "  [?] Choose inbound or outbound"
+read -p "  [?] Enter \"in\" or \"out\" ] => " target
+echo "";
+echo "--------------------------------------------";
+echo ""
+
 
 #if문으로 in을 선택 시, 출발지 라우팅 인터페이스를, out선택시, 목적지라우팅 인터페이스를 나머지는 확인하라고하기
-
-echo "";
-echo "   [+] interface = $target";
-
-# Go Tcpdump !!
+if [ $target == "in" ]; then
+  echo "  [IN] Dump Inbound Traffic ";
+elif [ $target == "out" ]; then
+  echo "  [OUT] Dump Outbound Traffic ";
+else
+  echo "  [!] syntex error => Enter \"in\" or \"out\" ";
+  echo ""
+  exit 0
+fi
+echo "  [=>] Start Tcpdump !!"
+echo ""
+echo "============================================";
+echo ""
+# start tcp dump
 dump="tcpdump -nni eth1 host $src_ip";
 $dump
